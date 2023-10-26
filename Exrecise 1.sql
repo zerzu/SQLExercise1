3ffa99154c957edb4685e60b71b6935eb16febf6
Eric Garner <ericgarner81@gmail.com>
2023-10-26 14:18:48 -0500

    completed exercise

---
+++ Exrecise 1.sql
@@ -0,0 +1,48 @@
+-- find all products
+
+SELECT * FROM Products;
+
+-- find all products that cost $1400
+SELECT * FROM Products
+WHERE Price IS 1400;
+
+-- find all products that cost $11.99 or $13.99
+SELECT * FROM Products
+WHERE Price IS 11.99, 13.99;
+
+-- find all products that do NOT cost 11.99 - using NOT
+SELECT * FROM Products
+WHERE Price NOT (11.99, 13.99);
+
+-- Find all products and sort them by price from greatest to least
+SELECT * FROM Products
+ORDER BY Price DESC;
+
+-- Find all employees who don't have a middle initial
+SELECT * FROM Employees
+WHERE MiddleInitial IS NULL;
+
+-- Find distinct product prices
+SELECT DISTINCT Price FROM Products;
+
+-- Find all employees whose first name starts with the letter ‘J’
+SELECT * FROM Employees
+WHERE FirstName LIKE 'J%';
+
+-- Find all Macbooks
+SELECT * FROM Products
+WHERE ProductName = 'Macbook';
+
+-- Find all products that are on sale
+SELECT * FROM Products
+WHERE IsOnSale = 1;
+
+-- Find the average price of all products
+SELECT AVG(Price) 
+AS AveragePrice 
+FROM Products;
+
+-- Find all Geek Squad employees who don't have a middle initial
+SELECT * FROM Employees
+WHERE MiddleInitial IS NULL 
+AND IsGeekSquad = 1;
